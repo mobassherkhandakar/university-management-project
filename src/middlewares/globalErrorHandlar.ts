2120b26dd348a67c1b9c21e576ac1b07e4951200
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-07-21 16:16:47 +0600

    finaly handlervalidation setup done

--- src/middlewares/globalErrorHandlar.ts
+++ src/middlewares/globalErrorHandlar.ts
@@ -1,17 +1,48 @@
-import { NextFunction, Request, Response } from 'express'
+/* eslint-disable prefer-const */
+import { ErrorRequestHandler } from 'express'
 import config from '../config'
 import { IGenericErrorMessage } from '../interfaces/error'
+import handleValidationError from './handleValidationError'
+import ApiError from '../errors/ApiError'
 
-const globalErrorHandler = (err,req: Request, res: Response, next: NextFunction) => {
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   let statusCode = 500
   let message = 'Something went wrong!'
   let errorMessage: IGenericErrorMessage[] = []
+
+  if (error.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessage = simplifiedError.errorMessage
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error?.message
+    errorMessage = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessage = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  }
 
   res.status(statusCode).json({
     success: false,
     message,
     errorMessage,
-    stack: config.env !== 'production' ? err?.stack : undefined,
+    stack: config.env !== 'production' ? error?.stack : undefined,
   })
   next()
 }
---
+++ src/middlewares/handleValidationError.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+import { IGenericErrorMessage } from '../interfaces/error'
+import { IGenericErrorResponse } from '../interfaces/common'
+
+const handleValidationError = (
+  err: mongoose.Error.ValidationError,
+): IGenericErrorResponse => {
+  const errors: IGenericErrorMessage[] = Object.values(err.errors).map(
+    (error: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
+      return {
+        path: error?.path,
+        message: error?.message,
+      }
+    },
+  )
+
+  const statusCode = 400
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorMessage: errors,
+  }
+}
+
+export default handleValidationError
