15be441b0a25a0b6b743de0dfe432f5f8de7ad5d
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-07-26 12:46:43 +0600

    academic semister router setup

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import validatedRequest from '../../middlewares/validationRequest';
+import { AcademicSemesterValidation } from './academicSemester.validation';
+import { AcademicSemesterController } from './academicSemester.controller';
 
 const router = express.Router();
-router.post('/create-user');
+router.post(
+  '/create-user',
+  validatedRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema),
+  AcademicSemesterController.createSemester,
+);
 
 export const UserRoute = router;
