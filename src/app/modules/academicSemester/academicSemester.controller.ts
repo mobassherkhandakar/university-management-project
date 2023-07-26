737ea9d2f66531536cc70591c40af852ba55b013
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-07-26 16:04:47 +0600

    cathc asycn function create

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,21 +1,18 @@
-import { RequestHandler } from 'express';
+import { Request, Response } from 'express';
 import { AcademicSemesterService } from './academicSemster.service';
+import catchAsync from '../../../shared/catchAsync';
 
-const createSemester: RequestHandler = async (req, res, next) => {
-  try {
-    const { ...academicSemesterData } = req.body;
-    const result = await AcademicSemesterService.createSemester(
-      academicSemesterData,
-    );
-    res.status(200).json({
-      success: true,
-      message: 'Semester Create Successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const createSemester = catchAsync(async (req: Request, res: Response) => {
+  const { ...academicSemesterData } = req.body;
+  const result = await AcademicSemesterService.createSemester(
+    academicSemesterData,
+  );
+  res.status(200).json({
+    success: true,
+    message: 'Semester Create Successfully',
+    data: result,
+  });
+});
 
 export const AcademicSemesterController = {
   createSemester,
--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -1,19 +1,16 @@
-import { RequestHandler } from 'express';
+import { Request, Response } from 'express';
 import { UserService } from './users.service';
+import catchAsync from '../../../shared/catchAsync';
 
-const createUser: RequestHandler = async (req, res, next) => {
-  try {
-    const { users } = req.body;
-    const result = await UserService.createUser(users);
-    res.status(200).send({
-      success: true,
-      message: 'user Created Successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const { users } = req.body;
+  const result = await UserService.createUser(users);
+  res.status(200).send({
+    success: true,
+    message: 'user Created Successfully',
+    data: result,
+  });
+});
 export const UserController = {
   createUser,
 };
